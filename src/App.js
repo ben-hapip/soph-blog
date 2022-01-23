d2e48dbbdb7d7ccf1578d3cf9d5dd00dd2d689d5
Sophie Jilek <sophiejilek@MacBook-Pro-4.local>
2022-01-23 17:22:41 -0600

    initial :D

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Homepage from './Homepage.js'
+function App() {
+  return (
+    <>
+    {/* // navbar */}
+  <Homepage> </Homepage>
+  </>
+  );
+}
+
+export default App;
