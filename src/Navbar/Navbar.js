afd8b92f15e2f603bc556c8f6b2241c3a03e691b
Ben Hapip <benhapipcodes@gmail.com>
2022-02-03 21:16:58 -0600

    rough navbar hehe

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,7 +1,36 @@
 import './Navbar.css';
+import { useState } from 'react';
 
 function Navbar() {
-  return <div>A FLAG WILL ALWAYS FLY</div>;
+  const navOptions = ['Home', 'Portfolio', 'Contact Me'];
+  const [dark, setDark] = useState(false);
+  const handleChange = () => {
+    setDark(!dark);
+  };
+  console.log(handleChange);
+  return (
+    <div className="nav-grid" style={{ backgroundColor: dark ? 'purple' : 'green' }}>
+      {navOptions.map((option, index) => (
+        <div key={index}>
+          <span>{option}</span>
+        </div>
+      ))}
+      <div className="toggle">
+        <input
+          checked={dark}
+          onChange={handleChange}
+          className="toggle-checkbox"
+          id={`react-switch-new`}
+          type="checkbox"
+        />
+        <label className="toggle-label" htmlFor={`react-switch-new`}>
+          <label>🌚</label>
+          <label>🌞</label>
+          <span className={`toggle-button`} />
+        </label>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
